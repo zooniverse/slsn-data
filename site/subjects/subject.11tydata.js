505835b55051e78985d8023fc831e32c6c01659f
Jim O'Donnell <jim@zooniverse.org>
2023-06-14 14:31:34 +0100

    Subject page metadata

--- site/subjects/subject.11tydata.js
+++ site/subjects/subject.11tydata.js
@@ -8,9 +8,25 @@ function subjectClassifications({ subject, classifications }) {
   return classifications.filter(c => c.subjectID == subject.id)
 }
 
+function ogImage({ subject }) {
+  const [lightCurve] = subject.locations
+  return lightCurve['image/jpeg']
+}
+
+function description({ subject, classifications }) {
+  const annotations = subjectClassifications({ subject, classifications }).map(c => {
+    return c.annotations.map(annotation => `${annotation.task}: ${annotation.value}`).join(' ')
+  })
+  return `
+    ZTF ID: ${subject.metadata.objectId}
+    ${annotations.join('\n')}
+  `
+}
 module.exports = {
   eleventyComputed: {
     title: subjectTitle,
-    subjectClassifications
+    subjectClassifications,
+    description,
+    ogImage
   }
 }
